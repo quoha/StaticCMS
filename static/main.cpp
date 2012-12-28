a3045a159b5b1bbec47735f3852ac9b85098c0c9
Michael Henderson <quoha@quoha.org>
2012-12-27 17:25:26 -0700

    Initial Commit

---
+++ static/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  static
+//
+//  Created by Michael Henderson on 12/27/12.
+//  Copyright (c) 2012 Michael Henderson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
